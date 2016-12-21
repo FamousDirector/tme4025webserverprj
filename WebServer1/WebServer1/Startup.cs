408bee08ae688cf7130fd3e804c5bc0e8a55125c
FamousDirector <jamesadcameron111@gmail.com>
2016-12-21 12:28:02 -0400

    added new project
    added a new project for the Web server

---
+++ WebServer1/WebServer1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebServer1.Startup))]
+namespace WebServer1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
