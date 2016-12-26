edff38ddd95331d1b7b4ccb8f39a7205efbaef89
FamousDirector <jamesadcameron111@gmail.com>
2016-12-25 23:54:11 -0400

    added multiview to main page
    still needs styling

--- WebServer1/WebServer1/Default.aspx.cs
+++ WebServer1/WebServer1/Default.aspx.cs
@@ -13,5 +13,58 @@ namespace WebServer1
         {
 
         }
+
+        protected void MultiView1_ActiveViewChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void NextButton_Click(object sender, EventArgs e)
+        {
+            // Determine which button was clicked
+            // and set the ActiveViewIndex property to
+            // the view selected by the user.
+            if (MainMultiView.ActiveViewIndex > -1 & MainMultiView.ActiveViewIndex <= 1)
+            {
+                // Increment the ActiveViewIndex property 
+                // by one to advance to the next view.
+                MainMultiView.ActiveViewIndex += 1;
+                MultiViewRadioButtons.SelectedIndex += 1;
+            }
+            else if (MainMultiView.ActiveViewIndex == 2)
+            {
+                //restart index
+                MainMultiView.ActiveViewIndex = 0;
+                MultiViewRadioButtons.SelectedIndex = 0;
+            }
+            else
+            {
+                throw new Exception("An error occurred.");
+            }
+        }
+        protected void BackButton_Click(object sender, EventArgs e)
+        {
+            if (MainMultiView.ActiveViewIndex > 0 & MainMultiView.ActiveViewIndex <= 2)
+            {
+                // Decrement the ActiveViewIndex property
+                // by one to return to the previous view.
+                MainMultiView.ActiveViewIndex -= 1;
+                MultiViewRadioButtons.SelectedIndex -= 1;
+            }
+            else if (MainMultiView.ActiveViewIndex == 0)
+            {
+                MainMultiView.ActiveViewIndex = 2;
+                MultiViewRadioButtons.SelectedIndex = 2;
+            }
+            else
+            {
+                throw new Exception("An error occurred.");
+            }
+        }
+
+        protected void MultiViewRadioButtons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MainMultiView.ActiveViewIndex = MultiViewRadioButtons.SelectedIndex;
+        }
     }
 }
--- WebServer1/WebServer1/Default.aspx.designer.cs
+++ WebServer1/WebServer1/Default.aspx.designer.cs
@@ -11,5 +11,68 @@ namespace WebServer1 {
     
     
     public partial class _Default {
+        
+        /// <summary>
+        /// MainMultiView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView MainMultiView;
+        
+        /// <summary>
+        /// View1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
+        
+        /// <summary>
+        /// View2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View2;
+        
+        /// <summary>
+        /// View3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View3;
+        
+        /// <summary>
+        /// MultiViewRadioButtons control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList MultiViewRadioButtons;
+        
+        /// <summary>
+        /// BackButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BackButton;
+        
+        /// <summary>
+        /// NextButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button NextButton;
     }
 }
