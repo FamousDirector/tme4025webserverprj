42008b5d2b8558f0398dfc294e4b15be1206d824
FamousDirector <jamesadcameron111@gmail.com>
2017-01-08 19:47:57 -0400

    added cookie to get users timezone

---
+++ WebServer1/WebServer1/ExtraCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer1
+{
+    public class ExtraCommands
+    {        public static int GetTimeZoneOffset(HttpRequest Request)
+        {
+            // Default to the server time zone
+            TimeZone tz = TimeZone.CurrentTimeZone;
+            TimeSpan ts = tz.GetUtcOffset(DateTime.Now);
+            int result = (int)ts.TotalMinutes;
+            // Then check for client time zone (minutes) in a cookie
+            HttpCookie cookie = Request.Cookies["ClientTimeZone"];
+            if (cookie != null)
+            {
+                int clientTimeZone;
+                if (int.TryParse(cookie.Value, out clientTimeZone))
+                    result = clientTimeZone;
+            }
+            return result;
+        }
+    }
+}
--- WebServer1/WebServer1/Site.Master.cs
+++ WebServer1/WebServer1/Site.Master.cs
@@ -127,7 +127,7 @@ namespace WebServer1
                 return "active";
             }
             return "";
-        }
+        }        
     }
 
 }
