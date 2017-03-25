96cf2f477f427d91f8117862be1f2f8c97081875
FamousDirector <jamesadcameron111@gmail.com>
2017-03-25 17:34:45 -0300

    disabled product page from toolbar

--- WebServer1/WebServer1/Products.aspx.designer.cs
+++ WebServer1/WebServer1/Products.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace WebServer1 {
         protected global::System.Web.UI.WebControls.View CustomerReview1;
         
         /// <summary>
+        /// CustomerReview2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View CustomerReview2;
+        
+        /// <summary>
         /// LastReivewButton control.
         /// </summary>
         /// <remarks>
